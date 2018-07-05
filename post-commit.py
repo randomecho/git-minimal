a2dca2309130fb31cbf21fd898ddbc5c807c0391
Soon Van <cog@randomecho.com>
2018-07-05 15:41:12 -0400

    Use two blank lines around top-level functions
    Per PEP 8:
    
    "Surround top-level function and class definitions with two blank lines."

--- post-commit.py
+++ post-commit.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 from textwrap import wrap
 import requests
 
+
 def get_page(url):
     response = requests.get(url)
 
@@ -15,6 +16,7 @@ def get_page(url):
     except requests.Timeout as e:
         return str(e)
 
+
 def get_extract(raw_html, html_tag, css_class):
     html = BeautifulSoup(raw_html, 'html.parser')
     extract = html.find(html_tag, attrs={'class': css_class})
@@ -24,12 +26,14 @@ def get_extract(raw_html, html_tag, css_class):
     else:
         return None
 
+
 def display_quote(quote, author):
     if quote is not None and author is not None:
         print()
         print("\n".join(wrap(quote, 70)))
         print(('{0:>70}').format(author))
         print()
+
 
 url = 'http://minimalmaxims.com/'
 html = get_page(url)
