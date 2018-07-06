4059f63da0f2b4011375b41d329a068b848e3315
Soon Van <cog@randomecho.com>
2018-07-06 11:38:12 -0400

    Create bash script version
    Does not and will not account for times when the quote contains
    tag soup like <I> or other formatting for those that seem to have it.

---
+++ post-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+html=$(wget -qO- http://minimalmaxims.com/)
+quote_regex='<span class="quotable-quote"><p>([a-zA-Z0-9 ;’,‘?:\.—\-]+)<\/p>'
+author_regex='<cite class="quoteable-author">([a-zA-Z0-9— ;\.\-]+)<\/cite>'
+
+if [[ $html =~ $quote_regex ]]; then
+    quote=${BASH_REMATCH[1]}
+else
+    echo $html
+fi
+
+if [[ $html =~ $author_regex ]]; then
+    author=${BASH_REMATCH[1]}
+fi
+
+echo
+echo $quote
+echo
+echo "     $author"
+echo
